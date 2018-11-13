ed19c2d2b705c838d64272d26e13cb8523842e93
Steve McCoole <steve.mccoole@objectpartners.com>
2018-11-13 12:56:32 -0600

    Renamte from eosio-core-framework to eosio-swift-common-framework.

--- EosioSwiftCommon/EosioCore.h
+++ EosioSwiftCommon/EosioCore.h
@@ -1,6 +1,6 @@
 //
-//  EosioCore.h
-//  EosioCore
+//  EosioSwiftCommon.h
+//  EosioSwiftCommon
 //
 //  Created by Steve McCoole on 11/6/18.
 //  Copyright © 2018 Block One. All rights reserved.
@@ -9,13 +9,13 @@
 #import <UIKit/UIKit.h>
 
 
-//! Project version number for EosioCore.
-FOUNDATION_EXPORT double EosioCoreVersionNumber;
+//! Project version number for EosioSwiftCommon.
+FOUNDATION_EXPORT double EosioSwiftCommonVersionNumber;
 
 //! Project version string for EosioCore.
-FOUNDATION_EXPORT const unsigned char EosioCoreVersionString[];
+FOUNDATION_EXPORT const unsigned char EosioSwiftCommonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements
-// like #import <EosioCore/PublicHeader.h>
+// like #import <EosioSwiftCommon/PublicHeader.h>
 #import "abieos.h"
 #import "base58.h"

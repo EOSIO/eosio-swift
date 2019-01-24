4eb74f5288068d61f7dbfe8652336df0d6ddf93f
Todd Bowden <toddhokie@gmail.com>
2019-01-24 17:08:02 -0500

    Empty framework project

---
+++ EosioSwift/EosioSwift.h
@@ -0,0 +1,19 @@
+//
+//  EosioSwift.h
+//  EosioSwift
+//
+//  Created by Todd Bowden on 1/24/19.
+//  Copyright © 2019 block.one. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EosioSwift.
+FOUNDATION_EXPORT double EosioSwiftVersionNumber;
+
+//! Project version string for EosioSwift.
+FOUNDATION_EXPORT const unsigned char EosioSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EosioSwift/PublicHeader.h>
+
+

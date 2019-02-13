f7871c3a6af26de42febc3dcfeb91f6a545c98f5
Todd Bowden <toddhokie@gmail.com>
2019-02-13 14:05:41 -0500

    add transaction properties

--- EosioSwift/EosioTransaction.swift
+++ EosioSwift/EosioTransaction.swift
@@ -7,10 +7,28 @@
 //
 
 import Foundation
+import EosioSwiftFoundation
 
 public class EosioTransaction {
     
+    public var chainId = ""
     
+    public var abis = [EosioName:String]()
+    
+    public var id: String?
+    public var blockNum: UInt64?
+    
+    public var expiration = Date(timeIntervalSince1970: 0)
+    public var refBlockNum:  UInt16 = 0
+    public var refBlockPrefix: UInt64 = 0
+    public var maxNetUsageWords: UInt = 0
+    public var maxCpuUsageMs: UInt = 0
+    public var delaySec: UInt = 0
+    public var contextFreeActions = [String]()
+    public var actions = [Action]()
+    public var transactionExtensions = [String]()
+    
+
     
     
 }

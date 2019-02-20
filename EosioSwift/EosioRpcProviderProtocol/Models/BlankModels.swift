8a1e848ce7c724f77837e160fba70f4c0a6b44d9
Steve McCoole <steve.mccoole@objectpartners.com>
2019-02-20 17:07:48 -0600

    Initial version of EosioRpcProviderProtocol. Includes supporting classes and structs.  Response objects are currently blank implementation stubs until we can get model generation sorted out. Request objects had already been hand written so they were included but they can be replaced when we can generate them from schema as well.

---
+++ EosioSwift/EosioRpcProviderProtocol/Models/BlankModels.swift
@@ -0,0 +1,75 @@
+//
+//  BlankModels.swift
+//  EosioSwift
+//
+//  Created by Steve McCoole on 2/20/19.
+//  Copyright © 2019 block.one. All rights reserved.
+//
+
+import Foundation
+
+public struct EosioRpcInfo: Codable {
+    
+}
+
+public struct EosioRpcBlock: Codable {
+    
+}
+
+public struct EosioRpcBlockHeaderState: Codable {
+    
+}
+
+public struct EosioRpcAccount: Codable {
+    
+}
+
+public struct EosioRpcAccountAbi: Codable {
+    
+}
+
+public struct EosioRpcRawAbi: Codable {
+    
+}
+
+public struct EosioRpcRawCodeAbi: Codable {
+    
+}
+
+public struct EosioRpcTableRows: Codable {
+    
+}
+
+public struct EosioRpcCurrencyBalance: Codable {
+    
+}
+
+public struct EosioRpcRequiredKeys: Codable {
+    
+}
+
+public struct EosioRpcCurrencyStats: Codable {
+    
+}
+
+public struct EosioRpcProducers: Codable {
+    
+}
+
+public struct EosioRpcTransaction: Codable {
+    
+}
+
+public struct EosioRpcHistoryActions: Codable {
+    
+}
+
+public struct EosioRpcKeyAccounts: Codable {
+    
+}
+
+public struct EosioRpcControllingAccounts: Codable {
+    
+}
+
+
---
+++ EosioSwift/EosioRpcProviderProtocol/Models/RequestModels.swift
@@ -0,0 +1,73 @@
+//
+//  RequestModels.swift
+//  EosioSwift
+//
+//  Created by Steve McCoole on 2/20/19.
+//  Copyright © 2019 block.one. All rights reserved.
+//
+
+import Foundation
+import EosioSwiftFoundation
+
+public struct EosioRpcTableRowsRequest: Codable {
+    var scope: String = "inita"
+    var code: String = "currency"
+    var table: String = "account"
+    var tableKey: String?
+    var json : Bool
+    var lowerBound : String?
+    var upperBound: String?
+    var limit : Int = 10
+    var indexPosition : String = "1"
+    var keyType: String?
+    var encodeType : String = "dec"
+    
+    enum CodingKeys: String, CodingKey {
+        case scope
+        case code
+        case table
+        case tableKey = "table_key"
+        case json
+        case lowerBound = "lower_bound"
+        case upperBound = "upper_bound"
+        case limit
+        case indexPosition = "index_position"
+        case keyType = "key_type"
+        case encodeType = "encode_type"
+    }
+}
+
+public struct EosioRpcCurrencyBalanceRequest: Codable {
+    var code: String
+    var account: String
+    var symbol: String
+}
+
+public struct EosioRpcRequiredKeysRequest: Codable {
+    var transactionJson: String
+    var availableKeys: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case transactionJson = "transaction"
+        case availableKeys = "available_keys"
+    }
+}
+
+public struct EosioRpcProducersRequest: Codable {
+    var limit: String
+    var lowerBound: String
+    var json: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case limit
+        case lowerBound = "lower_bound"
+        case json
+    }
+}
+
+public struct EosioRpcHistoryActionsRequest: Codable {
+    var position: Int32 = -1
+    var offset: Int32 = -20
+    var accountName: EosioName
+}
+

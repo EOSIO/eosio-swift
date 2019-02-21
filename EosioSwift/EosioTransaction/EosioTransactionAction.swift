8a2d6fb72cf84be781105eecd815a13c432a696c
Todd Bowden <toddhokie@gmail.com>
2019-02-21 16:43:40 -0500

    remove ricardian

--- EosioSwift/EosioTransaction/EosioTransactionAction.swift
+++ EosioSwift/EosioTransaction/EosioTransactionAction.swift
@@ -39,8 +39,6 @@ public extension EosioTransaction {
             return dataSerialized != nil
         }
         
-        /// The rendered ricardian contract and metadata for this action
-        public private(set) var ricardian: Ricardian?
         
         
         /// Coding keys
@@ -217,28 +215,5 @@ extension EosioTransaction.Action {
 }
 
 
-extension EosioTransaction.Action {
-    
-    /// Ricardian struct for `EosioTransaction.Action`
-    public struct Ricardian {
-        /// Rendered ricardian contract in html format
-        public var html = ""
-        /// Ricardian metadata (title, summary and icon)
-        public var metadata = Metadata()
-        /// Error rendering the ricardian contract
-        public var error = ""
-        
-        /// Ricardian metadata (title, summary and icon)
-        public struct Metadata {
-            /// Action title
-            public var title = ""
-            /// Action summary
-            public var summary = ""
-            /// Action icon url
-            public var icon = ""
-        }
-    }
-    
-}
 
 

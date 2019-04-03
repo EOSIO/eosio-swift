a7eb9436332ba51db9664cb7d85ec04a92b1b1da
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2019-04-03 09:27:30 -0400

    Fixed typo

--- EosioSwiftTests/EosioTransactionTests.swift
+++ EosioSwiftTests/EosioTransactionTests.swift
@@ -255,7 +255,7 @@ class RPCProviderMock: EosioRpcProviderProtocol {
                     packedContextFreeData: "lkj",
                     contextFreeData: ["kljl"],
                     packedTrx: "lkj",
-                    transaction: EosioRpcTransactionResponse(tranactionId: "mocktransactionid")
+                    transaction: EosioRpcTransactionResponse(transactionId: "mocktransactionid")
             )
             )
         ],

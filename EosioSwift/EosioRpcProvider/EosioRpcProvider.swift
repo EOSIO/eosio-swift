d72d824c5a9fb16c4548c7f47b8b0cdc2d9ed36a
Farid Rahmani <farid.harmani@block.one>
2019-04-04 11:17:51 -0400

    modified rpc implementation

--- EosioSwift/EosioRpcProvider/EosioRpcProvider.swift
+++ EosioSwift/EosioRpcProvider/EosioRpcProvider.swift
@@ -14,29 +14,41 @@ public struct EosioRpcProvider:EosioRpcProviderProtocol {
     public init(endpoint:URL){
         self.endpoint = endpoint
     }
-    public func getInfo(completion: @escaping (EosioResult<EosioRpcInfoResponse, EosioError>) -> Void) {
-        call(rpc: "chain/get_info", requestParameters: nil, callBack: completion)
+    public func getInfo(completion: @escaping (EosioResult<EosioRpcInfoResponseProtocol, EosioError>) -> Void) {
+        call(rpc: "chain/get_info", requestParameters: nil){(result: EosioRpcInfoResponse?, error:EosioError?) in
+            if let result = result{
+                completion(EosioResult.success(result))
+            }
+            
+            if let error = error{
+                completion(EosioResult.failure(error))
+            }
+        }
     }
-
-    public func getBlock(requestParameters: EosioRpcBlockRequest, completion: @escaping (EosioResult<EosioRpcBlockResponse, EosioError>) -> Void) {
+    
+    func gr<T>(r:EosioResult<T, EosioError>) {
+        
+    }
+    
+    public func getBlock(requestParameters: EosioRpcBlockRequest, completion: @escaping (EosioResult<EosioRpcBlockResponseProtocol, EosioError>) -> Void) {
         call(rpc: "chain/get_block", requestParameters: requestParameters, callBack: completion)
     }
-
-    public func getRawAbi(requestParameters: EosioRpcRawAbiRequest, completion: @escaping (EosioResult<EosioRpcRawAbiResponse, EosioError>) -> Void) {
+    
+    public func getRawAbi(requestParameters: EosioRpcRawAbiRequest, completion: @escaping (EosioResult<EosioRpcRawAbiResponseProtocol, EosioError>) -> Void) {
         call(rpc: "chain/get_raw_abi", requestParameters: requestParameters, callBack: completion)
     }
-
-    public func getRequiredKeys(requestParameters: EosioRpcRequiredKeysRequest, completion: @escaping (EosioResult<EosioRpcRequiredKeysResponse, EosioError>) -> Void) {
+    
+    public func getRequiredKeys(requestParameters: EosioRpcRequiredKeysRequest, completion: @escaping (EosioResult<EosioRpcRequiredKeysResponseProtocol, EosioError>) -> Void) {
         call(rpc: "chain/get_required_keys", requestParameters: requestParameters, callBack: completion)
     }
-
-    public func pushTransaction(requestParameters: EosioRpcPushTransactionRequest, completion: @escaping (EosioResult<EosioRpcTransactionResponse, EosioError>) -> Void) {
+    
+    public func pushTransaction(requestParameters: EosioRpcPushTransactionRequest, completion: @escaping (EosioResult<EosioRpcTransactionResponseProtocol, EosioError>) -> Void) {
         call(rpc: "chain/push_transaction", requestParameters: requestParameters, callBack: completion)
 
     }
-
-
-    private func call<T:Codable>(rpc:String, requestParameters:Encodable?, callBack:@escaping (EosioResult<T, EosioError>)->Void) {
+    
+    
+    private func call<T:Codable>(rpc:String, requestParameters:Encodable?, callBack:@escaping (T?, EosioError?)->Void) {
         let url = URL(string: "v1/" + rpc, relativeTo: endpoint)!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -45,34 +57,39 @@ public struct EosioRpcProvider:EosioRpcProviderProtocol {
                 let jsonData = try requestParameters.toJsonData(convertToSnakeCase: true)
                 request.httpBody = jsonData
             } catch {
-                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Error while encoding request parameters.", originalError: error as NSError)))
+//                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Error while encoding request parameters.", originalError: error as NSError)))
+                callBack(nil, EosioError(.rpcProviderError, reason: "Error while encoding request parameters.", originalError: error as NSError))
                 return
             }
         }
 
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error{
-                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Can't access network.", originalError: error as NSError)))
+//                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Can't access network.", originalError: error as NSError)))
+                callBack(nil, EosioError(.rpcProviderError, reason: "Can't access network.", originalError: error as NSError))
                 return
             }
 
             guard let httpResponse = response as? HTTPURLResponse else{
-                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Server didn't respond.", originalError: nil)))
+//                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Server didn't respond.", originalError: nil)))
+                callBack(nil, EosioError(.rpcProviderError, reason: "Server didn't respond.", originalError: nil))
                 return
             }
 
             guard (200...299).contains(httpResponse.statusCode) else{
-                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Server Error. Status Code: \(httpResponse.statusCode) Response: \(httpResponse.description)")))
+//                callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Server Error. Status Code: \(httpResponse.statusCode) Response: \(httpResponse.description)")))
+                callBack(nil, EosioError(.rpcProviderError, reason: "Server Error. Status Code: \(httpResponse.statusCode) Response: \(httpResponse.description)", originalError: nil))
                 return
             }
 
             if let data = data{
                 let decoder = JSONDecoder()
                 guard let resource = try? decoder.decode(T.self, from: data) else{
-                    callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Error decoding returned data.")))
+//                    callBack(EosioResult.failure(EosioError(.rpcProviderError, reason: "Error decoding returned data.")))
+                    callBack(nil, EosioError(.rpcProviderError, reason: "Error decoding returned data.", originalError: nil))
                     return
                 }
-                callBack(.success(resource))
+                callBack(resource, nil)
             }
         }
         task.resume()

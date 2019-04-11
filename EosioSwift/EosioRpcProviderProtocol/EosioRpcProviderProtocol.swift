f2801bef8320d55d0bcf6ca7852fa2c9b03598b4
Brandon Fancher <brandonfancher@gmail.com>
2019-04-11 18:08:15 -0400

    Docs for EosioRpcProviderProtocol and AllResponseProtocols.

--- EosioSwift/EosioRpcProviderProtocol/EosioRpcProviderProtocol.swift
+++ EosioSwift/EosioRpcProviderProtocol/EosioRpcProviderProtocol.swift
@@ -8,21 +8,32 @@
 
 import Foundation
 
+// Protocol defining the endpoints required by the core EOSIO SDK for Swift library.
 public protocol EosioRpcProviderProtocol {
 
-    /** Calls /v1/chain/get_info */
+    /// Calls /v1/chain/get_info.
+    ///
+    /// - Parameter completion: Completion called with an `EosioResult`.
     func getInfo(completion: @escaping(EosioResult<EosioRpcInfoResponseProtocol, EosioError>) -> Void)
 
-    /** Calls `/v1/chain/get_block` */
+    /// Calls /v1/chain/get_block.
+    ///
+    /// - Parameter completion: Completion called with an `EosioResult`.
     func getBlock(requestParameters: EosioRpcBlockRequest, completion: @escaping(EosioResult<EosioRpcBlockResponseProtocol, EosioError>) -> Void)
 
-    /** Calls `/v1/chain/get_raw_abi` */
+    /// Calls /v1/chain/get_raw_abi.
+    ///
+    /// - Parameter completion: Completion called with an `EosioResult`.
     func getRawAbi(requestParameters: EosioRpcRawAbiRequest, completion: @escaping(EosioResult<EosioRpcRawAbiResponseProtocol, EosioError>) -> Void)
 
-    /** Calls `/v1/chain/get_required_keys` */
+    /// Calls /v1/chain/get_required_keys.
+    ///
+    /// - Parameter completion: Completion called with an `EosioResult`.
     func getRequiredKeys(requestParameters: EosioRpcRequiredKeysRequest, completion: @escaping(EosioResult<EosioRpcRequiredKeysResponseProtocol, EosioError>) -> Void)
 
-    /** Calls `/v1/chain/push_transaction` */
+    /// Calls /v1/chain/push_transaction.
+    ///
+    /// - Parameter completion: Completion called with an `EosioResult`.
     func pushTransaction(requestParameters: EosioRpcPushTransactionRequest, completion: @escaping(EosioResult<EosioRpcTransactionResponseProtocol, EosioError>) -> Void)
 
 }

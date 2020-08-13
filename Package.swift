b617e73f3dfcbe35026f0b8f79f93ba86116b76a
Steve McCoole <steve.mccoole@objectpartners.com>
2020-08-13 10:38:09 -0500

    Set minimum OS X version.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "EosioSwift",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v11),
+	.macOS(.v10_13)
     ],
     products: [
         .library(

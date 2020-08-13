51a18d655e23c256fb9dbfa8ee4d799a17beb311
Steve McCoole <steve.mccoole@objectpartners.com>
2020-08-13 11:26:16 -0500

    Add OS X platform version.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "EosioSwiftEcc",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v11),
+        .macOS(.v10_13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.

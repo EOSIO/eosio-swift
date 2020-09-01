62d4904b7d877bce1b20a7c17d23e14c05b94dc8
Steve McCoole <steve.mccoole@objectpartners.com>
2020-09-01 14:03:31 -0500

    Update documentation generator script.

--- update_documentation.sh
+++ update_documentation.sh
@@ -7,3 +7,6 @@ jazzy --module EosioSwiftAbieosSerializationProvider --swift-build-tool spm --bu
 
 echo "Updating documentation for EosioSwiftEcc..."
 jazzy --module EosioSwiftEcc --swift-build-tool spm --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 --clean --theme fullwidth --github_url https://github.com/EOSIO/eosio-swift --github-file-prefix https://github.com/EOSIO/eosio-swift/tree/master --hide-documentation-coverage --undocumented-text "" --copyright "Copyright (c) 2017-2020 block.one and its contributors. All rights reserved." --readme README_Ecc.md --output docs/EosioSwiftEcc
+
+echo "Updating documentation for EosioSwiftSoftkeySignatureProvider..."
+jazzy --module EosioSwiftSoftkeySignatureProvider --swift-build-tool spm --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 --clean --theme fullwidth --github_url https://github.com/EOSIO/eosio-swift --github-file-prefix https://github.com/EOSIO/eosio-swift/tree/master --hide-documentation-coverage --undocumented-text "" --copyright "Copyright (c) 2017-2020 block.one and its contributors. All rights reserved." --readme README_SoftkeySignatureProvider.md --output docs/EosioSwiftSoftkeySignatureProvider

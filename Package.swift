e8669ff651c2f491b2df46a4c6756f64e1da863f
Steve McCoole <steve.mccoole@objectpartners.com>
2020-09-01 11:08:51 -0500

    Add documentation generation for multiple modules.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,13 +6,18 @@ import PackageDescription
 let package = Package(
     name: "EosioSwift",
     platforms: [
-        .iOS(.v11),
-	.macOS(.v10_13)
+        .iOS(.v12),
+	.macOS(.v10_14)
     ],
     products: [
         .library(
             name: "EosioSwift",
-            targets: ["EosioSwift"])
+            targets: ["EosioSwift"]
+        ),
+        .library(
+            name: "EosioSwiftAbieosSerializationProvider",
+            targets: ["Abieos", "EosioSwiftAbieosSerializationProvider"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
@@ -28,12 +33,80 @@ let package = Package(
                 "PromiseKit",
                 "PMKFoundation",
                 "BigInt"
-            ]),
+            ],
+            path: "Sources/EosioSwift"
+        ),
+        .target(
+            name: "Abieos",
+            dependencies: [],
+            path: "Sources/Abieos",
+            /*
+                SPM 5.3 requires everything in the Sources directory to be a known type, declared as a
+                resource or excluded.  Unfortunately 5.3 does not recognize hpp headers as valid source
+                files.  The compiler still finds them as they are included by cpp files though.  Temp
+                work around is to exclude them.  See this thread for more information:
+                https://forums.swift.org/t/spm-packagedescription-5-3-does-not-recognize-hpp-headers-as-such-and-asks-for-them-to-be-excluded-or-used-as-resources/38466
+            */
+            exclude: [
+                "abieos.hpp",
+                "abieos_exception.hpp",
+                "abieos_numeric.hpp",
+                "abieos_ripemd160.hpp",
+                "eosio/abi.hpp",
+                "eosio/asset.hpp",
+                "eosio/bytes.hpp",
+                "eosio/chain_conversions.hpp",
+                "eosio/chain_types.hpp",
+                "eosio/check.hpp",
+                "eosio/convert.hpp",
+                "eosio/crypto.hpp",
+                "eosio/fixed_bytes.hpp",
+                "eosio/float.hpp",
+                "eosio/for_each_field.hpp",
+                "eosio/from_bin.hpp",
+                "eosio/from_json.hpp",
+                "eosio/from_string.hpp",
+                "eosio/murmur.hpp",
+                "eosio/name.hpp",
+                "eosio/opaque.hpp",
+                "eosio/operators.hpp",
+                "eosio/reflection.hpp",
+                "eosio/ship_protocol.hpp",
+                "eosio/stream.hpp",
+                "eosio/symbol.hpp",
+                "eosio/time.hpp",
+                "eosio/to_bin.hpp",
+                "eosio/to_json.hpp",
+                "eosio/to_key.hpp",
+                "eosio/types.hpp",
+                "eosio/varint.hpp",
+                "eosio/fpconv.license",
+                "LICENSE.txt"
+            ]
+        ),
+        .target(
+            name: "EosioSwiftAbieosSerializationProvider",
+            dependencies: ["Abieos", "EosioSwift"],
+            path: "Sources/EosioSwiftAbieosSerializationProvider",
+            resources: [
+                .copy("abi.abi.json"),
+                .copy("eosio.assert.abi.json"),
+                .copy("transaction.abi.json")
+            ]
+        ),
         .testTarget(
             name: "EosioSwiftTests",
             dependencies: [
                 "EosioSwift",
                 .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs")
-            ])
-    ]
+            ],
+            path: "Tests/EosioSwiftTests"
+        ),
+        .testTarget(
+            name: "EosioSwiftAbieosSerializationProviderTests",
+            dependencies: ["EosioSwiftAbieosSerializationProvider"],
+            path: "Tests/EosioSwiftAbieosSerializationProviderTests"
+        ),
+    ],
+    cxxLanguageStandard: .cxx1z
 )
